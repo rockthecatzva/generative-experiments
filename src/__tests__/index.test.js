7c809b890d214f847ebacf445a4466bad4bcdbed
Dan Catzva <dcatzva@gmail.com>
2019-04-30 14:35:05 +0000

    Added jest for testing and a folder with one initial fake test

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,4 @@
+test("it exists", () => {
+  const one = 1;
+  expect(one).toBe(1);
+});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-console.log("started");
+console.log("started 1234");
 const arr = [1, 2, 3];
 const iAmJavascriptES6 = () => console.log(...arr);
 iAmJavascriptES6();
